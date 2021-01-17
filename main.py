0c084f9620afeb226c9284f9bdaa65cf9796ddb6
Likhon Leo Gomes <tug60499@temple.edu>
2021-01-16 22:08:34 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,73 @@
+
+
+
+
+
+
+
+
+"""An example of how to setup and start an Accessory.
+This is:
+1. Create the Accessory object you want.
+2. Add it to an AccessoryDriver, which will advertise it on the local network,
+    setup a server to answer client queries, etc.
+"""
+from neopixel import *
+from NeoPixelLightStrip import NeoPixelLightStrip
+import logging
+import signal
+import random
+
+from pyhap.accessory import Accessory, Bridge
+from pyhap.accessory_driver import AccessoryDriver
+import pyhap.loader as loader
+from pyhap import camera
+from pyhap.const import CATEGORY_SENSOR
+
+logging.basicConfig(level=logging.INFO, format="[%(module)s] %(message)s")
+
+
+class TemperatureSensor(Accessory):
+    """Fake Temperature sensor, measuring every 3 seconds."""
+
+    category = CATEGORY_SENSOR
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        serv_temp = self.add_preload_service('TemperatureSensor')
+        self.char_temp = serv_temp.configure_char('CurrentTemperature')
+
+    @Accessory.run_at_interval(3)
+    async def run(self):
+        self.char_temp.set_value(random.randint(18, 26))
+
+
+def get_bridge(driver):
+    """Call this method to get a Bridge instead of a standalone accessory."""
+    bridge = Bridge(driver, 'Bridge')
+    temp_sensor = NeoPixelLightStrip( 8,True, 18, 800000, 10, 255, False, driver, 'Sensor 1')
+    #temp_sensor2 = TemperatureSensor(driver, 'Sensor 1')
+    bridge.add_accessory(temp_sensor)
+    #bridge.add_accessory(temp_sensor2)
+
+    return bridge
+
+
+def get_accessory(driver):
+    """Call this method to get a standalone Accessory."""
+    return NeoPixelLightStrip( 8,True, 18, 800000, 10, 255, False, driver, 'Sensor 1')
+
+
+# Start the accessory on port 51826
+driver = AccessoryDriver(port=51826)
+
+# Change `get_accessory` to `get_bridge` if you want to run a Bridge.
+driver.add_accessory(accessory=get_accessory(driver))
+
+# We want SIGTERM (terminate) to be handled by the driver itself,
+# so that it can gracefully stop the accessory, server and advertising.
+signal.signal(signal.SIGTERM, driver.signal_handler)
+
+# Start it!
+driver.start()
